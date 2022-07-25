6c193a3aaede28ade4c6f026b11ededa559e017f
mororong9 <kangmoonju0603@gmail.com>
2022-07-25 14:01:00 +0900

    print logic concept

---
+++ Beakjoon_python/print/4print_10953.py
@@ -0,0 +1,5 @@
+r= int(input())
+for i in range(r):
+    a,b=map(int, input().split(', '))
+    result=a+b
+    print(result)
---
+++ Beakjoon_python/print/6print_11022.py
@@ -0,0 +1,5 @@
+r= int(input())
+for i in range(1, r+1):
+    a,b= map(int, input().split())
+    result=a+b
+    print(f'Case #{i}: {a} + {b} = {result}')
